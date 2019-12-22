5895d3859e12110cac63c1d6cd2652b4a0200049
Michael <ycamic@gmail.com>
2019-12-22 22:43:18 +0200

    Add AppointmentsDayView component

--- src/Appointment.js
+++ src/Appointment.js
@@ -1,3 +1,19 @@
 import React from 'react';
 
-export const Appointment = ({customer}) => <div>{customer.firstName}</div>;
+export const Appointment = ({ customer }) => <div>{customer.firstName}</div>;
+
+const appointmentTimeOfDay = startsAt => {
+    const [h, m] = new Date(startsAt).toTimeString().split(':');
+
+    return `${h}:${m}`;
+}
+
+export const AppointmentsDayView = ({ appointments }) => (
+    <div id="appointmentsDayView">
+        <ol>
+            {appointments.map(appointment => {
+                return <li key={appointment.startsAt}>{appointmentTimeOfDay(appointment.startsAt)}</li>
+            })}
+        </ol>
+    </div>
+);
