328077e9879b3ef139632ec6c16da98c32d2a2fa
Michael <ycamic@gmail.com>
2019-12-27 23:53:36 +0200

    Submit the form with data

--- src/CustomerForm.js
+++ src/CustomerForm.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
-export const CustomerForm = ({ firstName }) => (
-    <form id="customer">
+export const CustomerForm = ({ firstName, onSubmit }) => {
+    const customer = { firstName };
+    return <form id="customer" onSubmit={() => onSubmit(customer)}>
         <label htmlFor="firstName">First name</label>
         <input readOnly id="firstName" name="firstName" type="text" value={firstName} />
     </form>
-);
+};
